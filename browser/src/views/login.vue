03b789d728f4223550a3902fb9419c54a67a1c5e
仁焜 <1051455824@qq.com>
2016-09-05 22:26:59 +0800

    a

--- browser/src/views/login.vue
+++ browser/src/views/login.vue
@@ -1,17 +1,9 @@
 <style scoped>
 
-.body {
-    background: #fff;
-}
-
 </style>
 
 <template>
 
-<h1>list</h1>
-
-<img src="../assets/logo.png" />
-<bottom-tab-bar></bottom-tab-bar>
 
 </template>
 
@@ -25,11 +17,11 @@ import {
     incrementCounter, fetchname
 }
 from '../vuex/actions';
-import bottomTabBar from '../components/bottomTabBar.vue';
+
 
 module.exports = {
     components: {
-        'bottom-tab-bar': bottomTabBar
+      
     },
 }
 
